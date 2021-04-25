ab424fc71c881b07c539ee8d47cb1290ceea8347
Steven Tan <steven@steventan.com.au>
2021-04-25 16:01:50 +1000

    align ratings with hotel title

--- src/components/hotel/Details.vue
+++ src/components/hotel/Details.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="hotel-details">
+    <div class="row">
+      <h3>{{ title }}</h3>
 
-    <h3>{{ title }}</h3>
-
-    <Rating
-      :type="ratingType"
-      :value="ratingValue" />
+      <Rating
+        :type="ratingType"
+        :value="ratingValue" />
+    </div>
 
     <div class="address">{{ address }}</div>
 
@@ -38,8 +39,14 @@ export default {
   width: 100%;
   padding: 1rem;
 }
+.row {
+  padding-top: 0;
+  justify-content: start;
+  align-items: center;
+  flex-wrap: wrap;
+}
 h3 {
-  margin: 0;
+  margin: 0 1rem 0 0;
   font-weight: 600;
 }
 .address {
