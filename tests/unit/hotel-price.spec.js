37903e02a0fc094e29d1514854126162d1ac813a
Steven Tan <steven@steventan.com.au>
2021-04-25 19:14:55 +1000

    refactor hotel-price.spec

--- tests/unit/hotel-price.spec.js
+++ tests/unit/hotel-price.spec.js
@@ -3,7 +3,7 @@ import mockData from '../../public/api/data.json'
 import { storeModule } from "../../src/store";
 import { PRICE_HIGH_LOW } from '../../src/enums/order-by'
 import dataProcess from '../../src/modules/data/process'
-import HotelItem from '@/components/Hotel/Item.vue'
+import HotelPrice from '@/components/Hotel/Price.vue'
 
 let state, hotel, wrapper
 
@@ -14,28 +14,33 @@ beforeEach(() => {
   }
 })
 
+function componentMount(h) {
+  return mount(HotelPrice, {
+    propsData: {
+      price: h.offer.displayPrice.amount,
+      priceCurrency: h.offer.displayPrice.currency,
+      savings: h.offer.savings.amount,
+      savingsCurrency: h.offer.savings.currency
+    }
+  })
+}
+
 describe('Hotel price', () => {
   it('renders the savings amount if exists', () => {
     hotel = storeModule.getters.listing(state)[1]
-    wrapper = mount(HotelItem, {
-      propsData: { hotel }
-    })
+    wrapper = componentMount(hotel)
     expect(wrapper.find('.hotel-price .savings').exists()).toBe(true)
   })
 
   it('displays the saving amount text if exists', () => {
     hotel = storeModule.getters.listing(state)[1]
-    wrapper = mount(HotelItem, {
-      propsData: { hotel }
-    })
+    wrapper = componentMount(hotel)
     expect(wrapper.find('.hotel-price .savings').text()).toBe('Save $28~')
   })
 
   it('hides the savings amount if it is null', () => {
     hotel = storeModule.getters.listing(state)[0]
-    wrapper = mount(HotelItem, {
-      propsData: { hotel }
-    })
+    wrapper = componentMount(hotel)
     expect(wrapper.find('.hotel-price .savings').exists()).toBe(false)
   })
 })
